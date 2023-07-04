ffc892fc6f1a17ae182193f9137d602e23541368
lpcastilla <128614607+lpcastilla@users.noreply.github.com>
2023-07-04 21:51:51 +0000

    se agrego toDo.jsx y se modifico index.css

---
+++ src/js/component/toDo.jsx
@@ -0,0 +1,54 @@
+import React , {useState} from "react";
+
+export const ToDo = () => {
+	const [data, setData] = useState ("");
+	const [toDo, setToDo] = useState ([]);
+    return (
+        <div className="container d-flex flex-column align-items-center justify-content-center mt-5">
+			<h2 className="text-center text-secondary">To Do List</h2>
+			<div className="input col-sm-6">
+				<input 
+					type="text" 
+					className="form-control" 
+					placeholder="What needs to be done?" 
+					value={data}
+					onChange={(event) => {
+						//console.log(data);
+						setData(event.target.value);
+						}	
+					}
+					onKeyDown={(event) => {
+						//console.log(event.key);
+						if (event.key == "Enter"){
+							setToDo((prev) => {
+								return [...prev , data]
+							})
+						}
+					}}
+				/>
+			</div>
+			<ul className="list-group col-sm-6">
+				{toDo.map((item, index) => {
+					return (
+						<li key={index} className="list-group-item d-flex justify-content-between">
+							<span>{item}</span>
+							<button 
+								className="btn btn-light p-0" 
+								onClick={() => {
+									const filteredItems = toDo.filter((_, filteredIndex) => filteredIndex !== index);
+									setToDo(filteredItems);
+								}}
+							>
+								<i className="fa-solid fa-xmark"></i>
+							</button>
+						</li>
+					);
+				})}
+				<li className="list-group-item text-secondary">{toDo.length} item left</li>
+			</ul>
+		</div>
+    );
+};
+
+
+
